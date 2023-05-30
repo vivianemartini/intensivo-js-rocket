184f6a17a0d98051b241a7af4413cfd83d7e0b4c
Viviane Martini <martini.viviane@gmail.com>
2023-05-29 23:30:59 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+//1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+console.log('Hello World')
+
+//2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+let name = 'Viviane'
+let lastName = 'Martini'
+
+console.log(name + ' ' + lastName)
+
+
+//3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem 
+//"É um número", caso contrário, exiba a mensagem "Não é um número".
+// Para saber o tipo de dado você pode usar o operador `typeof`
+
+
+
+
+    
+//4. Crie um script que declare uma variável e verifique se o seu valor é uma string. 
+//Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+
+
+
+
+//5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. 
+//Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+
+
+
+//6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+
+
+//7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+
+//8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+
+//9. Crie um script que declare uma variável e verifique se o seu valor é um número par. 
+//Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+
+
+//10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. 
+//Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
